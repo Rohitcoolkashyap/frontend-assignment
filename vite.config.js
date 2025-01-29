236ea546e338e2e0cd82fe81ddd0e686b46e418a
rohit2.kashyap <rohit2.kashyap@paytm.com>
2025-01-29 15:44:01 +0530

    master: pushed initial changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'esbuild',
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
